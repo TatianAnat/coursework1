8bc671a997a5dce4f06d18fefe9e896008fcd2b5
TatianAnat <TatianAnat@yandex.ru>
2025-05-17 22:25:52 +0400

    Employee

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,24 @@ public class Main {
 
             public Employee(String fullName, int department, int salary){
                 id = idСounter ++;
+                this.fullName = fullName;
+                this.department = department;
+                this.salary = salary;
             }
+
+            public void setDepartment(int departament){
+                this.department = departament;
+            }
+            public void setSalary(int salary){
+                this.salary = salary;
+            }
+            public int getId() {
+                return id;
+            }
+            public String getFullName(){
+                return fullName;
+            }
+
         }
     }
 }
